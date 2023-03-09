b89149f76d2041304314dea395546b91d4475505
Yuliia Ostrovska <ostrovskajuli@gmai.com>
2023-03-09 15:48:02 +0200

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,45 @@
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+const createGalleryMarcUp = () => galleryItems.map((image) =>
+    `<div class="gallery__item">
+        <a class="gallery__link" href="${image.original}">
+            <img
+            class="gallery__image"
+            src="${image.preview}"
+            data-source="${image.original}"
+            alt="${image.description}"
+            />
+        </a>
+    </div>`
+).join("");
+    
+
+galleryContainer.insertAdjacentHTML('beforeend', createGalleryMarcUp());
+
+galleryContainer.addEventListener('click', onImage);
+
+function onImage(evt) {
+    evt.preventDefault();
+    console.log(evt.target.nodeName)
+    if (evt.target.nodeName !== "IMG") {
+        return;
+    };
+    const imageSelected = evt.target.getAttribute("data-source");
+    if (!imageSelected){return;}
+    console.log(imageSelected);
+
+    new SimpleLightbox('.gallery a', { captionDelay: 250, captionPosition: 'bottom', docClose: true});
+        
+    
+};
